0f6d8bea63c2e87b0801255bd5a93e67547c6226
Mahoney96 <cmahoney966@gmail.com>
2020-05-30 17:18:48 -0700

    Updated React Prac Saturday

---
+++ app-3/src/arrowFunctionsPrac.js
@@ -0,0 +1,25 @@
+function sum(a, b){
+    return a + b
+}
+
+let sum2 = (a, b) => a + b
+
+
+
+function isPositive(number) {
+    return number >= 0
+}
+
+let isPositive2 = number => number >= 0
+
+//When you have a single parameter arrow function (=>) like the function above, where the only parameter is ' number ' , you are able to write the function without needing any () around the single parameter. Will need if mult parameters used in the function.
+
+let exampleArrow = arrow => arrow >= 0
+
+
+
+function randomNumber(){
+    return Math.random
+}
+
+document.addEventListener('click', => () console.log('click'))
